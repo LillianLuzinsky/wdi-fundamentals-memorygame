5d719230796bb8319a45f4962bdca2e5181af6f3
Lillian_Luzinsky <eyeris@tpg.com.au>
2019-08-05 16:00:40 +1000

    fixed game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,19 +5,52 @@ var cardsInPlay = [];
 cardsInPlay.push('cards[cardId]');
 
 function checkForMatch(){
-	if(cardsInPlay.length === 2){
-		if(cardsInPlay[0] === cardsInPlay[1]){
+	if (cardsInPlay[0] === cardsInPlay[1]){
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again.");
 		}
-	};
 };
 
-function flipCard(cardId){
+function flipCard (cardId){
 	console.log("User flipped " + cards[cardId]);
-	checkForMatch();	
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
 };
 
 flipCard(0);
 flipCard(2);
+
+
+//object
+
+/*var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
+*/
+
+
